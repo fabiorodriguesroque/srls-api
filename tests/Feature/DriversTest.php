286b6ab400adaffb454c7ce08949443986d13d4e
fabiorodriguesroque <fabioroque9@gmail.com>
2024-03-12 22:17:54 +0000

    test: add todo tests

---
+++ tests/Feature/DriversTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Drivers
+ *  1. create
+ *  2. read all
+ *  3. read
+ *  4. update
+ *  5. delete
+ * 
+ */
+
+test('Authenticated user can create drivers to his own league.', function () {})->todo();
+
+test('Authenticated user can not create drivers to other leagues.', function () {})->todo();
+
+test('Authenticated user can read all drivers from any league.', function () {})->todo();
+
+test('Authenticated user can read one driver from any league.', function () {})->todo();
+
+test('Authenticated user can update drivers from his own league.', function () {})->todo();
+
+test('Authenticated user can not update drivers from other leagues.', function () {})->todo();
+
+test('Authenticated user can delete drivers from his own league.', function () {})->todo();
+
+test('Authenticated user can not delete drivers from his own league.', function () {})->todo();
+
+
